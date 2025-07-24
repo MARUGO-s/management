8b439f233d715d3a78af590428a7266fc86b24b8
MARUGO-s <pingus0428@gmail.com>
2025-07-24 11:25:08 +0900

    Update main.js
    Signed-off-by: MARUGO-s <pingus0428@gmail.com>

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxv3Ssyc4VTniJCVCNOuFHJrYN8OYYUkKiI7N573IjqHP9RvtIkQzPTtBdx9JRlS5C8/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwV3Rerqq183yMAon3LOxgWJp80vhlA8HdcxtQMjxVmDvD2bQI-IxI0UNpCzgXc1Uv8/exec";
 const shops = [
   "MARUGO‑D", "MARUGO‑OTTO", "元祖どないや新宿三丁目", "鮨こるり",
   "MARUGO", "MARUGO2", "MARUGO GRANDE", "MARUGO MARUNOUCHI",
