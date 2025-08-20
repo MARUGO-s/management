2d3c39a660193e3ca69fc6019b525ec50ff67330
MARUGO-s <pingus0428@gmail.com>
2025-08-20 15:42:26 +0900

    Update correction.js
    Signed-off-by: MARUGO-s <pingus0428@gmail.com>

--- correction.js
+++ correction.js
@@ -8,7 +8,7 @@ function hideSubmitOverlay() {
   if (el) { el.style.display = 'none'; el.setAttribute('aria-hidden','true'); }
 }
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzoNdSD...RmsbydJUsHsgAaJmGKvzcc8_bA_XHXBf3Ee-ltRxLwhzzYVpU3NdCnK2d/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwB5beugBiPvXJmTu0T9XGDH8w86BBPrIyDe32zeoo6aJOKV6vvUChN3N8-A-PsXCQ/exec";
 const shops = [
   "MARUGO-D", "MARUGO-OTTO", "元祖どないや新宿三丁目", "鮨こるり",
   "MARUGO", "MARUGO2", "MARUGO GRANDE", "MARUGO MARUNOUCHI",
