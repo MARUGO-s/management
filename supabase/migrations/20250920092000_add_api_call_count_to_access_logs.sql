303f7fd1e954af324234e1c851713496d178aefa
MARUGO-s <pingus0428@gmail.com>
2025-09-25 04:23:53 +0900

    Delete supabase directory
    Signed-off-by: MARUGO-s <pingus0428@gmail.com>

--- supabase/migrations/20250920092000_add_api_call_count_to_access_logs.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE access_logs
-    ADD COLUMN IF NOT EXISTS api_call_count INTEGER DEFAULT 0;
-
-UPDATE access_logs
-SET api_call_count = 0
-WHERE api_call_count IS NULL;
--- supabase/migrations/20250924070000_change_quantity_to_text.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE recipe_ingredients
-ALTER COLUMN quantity TYPE TEXT;
